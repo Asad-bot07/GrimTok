4ecb33fe1dec14da6d87709f25bd0fb49cd687d5
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-21 23:05:27 +0530

    browser router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,30 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import OutputPage from './OutputPage'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import OutputPage from "./OutputPage";
 // import { OutputPage } from './Components/Output'
-import { UserContextProvider } from './Context/UserContextProvider'
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route>
-      <Route path='' element={<App/>} />
-      <Route path='/output' element={<OutputPage/>} />
-  </Route>
-))
+import { UserContextProvider } from "./Context/UserContextProvider";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <BrowserRouter basename="/GrimTok">
+      <Route>
+        <Route path="" element={<App />} />
+        <Route path="/output" element={<OutputPage />} />
+      </Route>
+    </BrowserRouter>
+  )
+);
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <UserContextProvider>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </UserContextProvider>
-)
+);
