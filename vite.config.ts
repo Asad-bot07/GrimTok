6620363448703d7ad4bccd07286455ecef1dd2a6
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-21 23:17:19 +0530

    browser router added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
-    outDir: "build", // change output folder
+    outDir: "build", // output folder for gh-pages
   },
-  base: "/GrimTok/",
+  base: "/GrimTok/", // important for GitHub Pages
 });
