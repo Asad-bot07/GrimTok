dacc792b489b192c6a41c2ebc3776c9131f4ce8a
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-21 23:12:27 +0530

    browser router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,17 @@
-// import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
 import OutputPage from "./OutputPage";
-// import { OutputPage } from './Components/Output'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserContextProvider } from "./Context/UserContextProvider";
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <BrowserRouter basename="/GrimTok">
-      <Route>
-        <Route path="" element={<App />} />
-        <Route path="/output" element={<OutputPage />} />
-      </Route>
-    </BrowserRouter>
-  )
-);
 
 createRoot(document.getElementById("root")!).render(
   <UserContextProvider>
-    <RouterProvider router={router} />
+    <BrowserRouter basename="/GrimTok">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/output" element={<OutputPage />} />
+      </Routes>
+    </BrowserRouter>
   </UserContextProvider>
 );
